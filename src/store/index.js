2a23bc7103ec957ff32d13cccfeb1f14dd55d2d1
glenncai <glenncai.work@gmail.com>
2021-08-18 17:02:27 +0800

    feat: hide snackbar

--- src/store/index.js
+++ src/store/index.js
@@ -55,6 +55,9 @@ export default new Vuex.Store({
         state.snackbar.show = true
         state.snackbar.text = text
       }, timeout);
+    },
+    hideSnackbar(state) {
+      state.snackbar.show = false
     }
   },
   actions: {
