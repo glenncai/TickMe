fc2b962c6faf309c58f49ea70bd99003083fa423
glenncai <glenncai.work@gmail.com>
2021-08-18 12:33:51 +0800

    feat: move tasks into vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,29 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    tasks: [
+      {
+        id: 1,
+        title: 'Wake up',
+        done: false
+      },
+      {
+        id: 2,
+        title: 'Eat bananas',
+        done: false
+      },
+      {
+        id: 3,
+        title: 'Go to run',
+        done: false
+      }
+    ]
   },
   mutations: {
   },
   actions: {
   },
-  modules: {
+  getters: {
+
   }
 })
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -12,12 +12,12 @@
       clearable
     ></v-text-field>
     <v-list
-      v-if="tasks.length"
+      v-if="$store.state.tasks.length"
       class="pt-0"
       flat
     >
       <div
-        v-for="task in tasks"
+        v-for="task in $store.state.tasks"
         :key="task.id"
       >
         <v-list-item
@@ -73,8 +73,7 @@ export default {
   name: 'Todo',
   data() {
     return {
-      newTaskTitle: '',
-      tasks: []
+      newTaskTitle: ''
     }
   },
   methods: {
