c4c7983f01435741b10e1d0583fa9fde51dbd6e2
glenncai <glenncai.work@gmail.com>
2021-08-18 10:18:59 +0800

    feat: add data property for the tasks

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,37 +1,9 @@
 <template>
-  <div class="about pa-6">
-    <h1>About Tick Me</h1>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
+	<div class="about pa-6">
+	<h1>About Tick Me</h1>
+	<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
+	<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
+	<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
+	<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tenetur maiores quae, officiis optio molestias illo tempore at doloribus consectetur ex perferendis iste suscipit aliquam iure ad. Aperiam, alias expedita.</p>
   </div>
 </template>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,6 +1,30 @@
 <template>
-  <div class="pa-6">
-    <h1>Todo Page</h1>
+  <div>
+    <v-list
+      class="pt-0"
+      flat
+    >
+      <div
+        v-for="task in tasks"
+        :key="task.id"
+      >
+        <v-list-item>
+          <template v-slot:default="{ active, }">
+            <v-list-item-action>
+              <v-checkbox
+                :input-value="active"
+                color="primary"
+              ></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ task.title }}</v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+    </v-list>
   </div>
 </template>
 
@@ -8,5 +32,23 @@
 
 export default {
   name: 'Todo',
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: 'Wake up'
+        },
+        {
+          id: 2,
+          title: 'Eat bananas'
+        },
+        {
+          id: 3,
+          title: 'Go to run'
+        },
+      ]
+    }
+  }
 }
 </script>
